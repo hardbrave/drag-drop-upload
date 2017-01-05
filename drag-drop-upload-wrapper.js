d2c5f35f13f0f449ada0f5f3373fd3b9e42a2392
qianli <qianli@1dakou.com>
2017-01-05 13:05:43 +0800

    initial commit

---
+++ drag-drop-upload-wrapper.js
@@ -0,0 +1,80 @@
+(function ($) {
+    var sanitized_options = function(opts) {
+        var default_options = {
+            image_type: 'article',
+            image_desc_sel: '',
+            image_url_sel: '',
+            upload_callback: void 0,
+            dragenter_css_callback: function () {
+                $(this).css('border', '2px solid #0B85A1');
+                $(this).text('松开图片进行上传');
+            },
+            dragover_css_callback: function () {
+                $(this).css('border', '2px solid #0B85A1');
+                $(this).text('或将图片拖拽到此处直接上传');
+            },
+            drop_css_callback: function () {
+                $(this).css('border', '2px dotted #0B85A1');
+            },
+            uploading_css_callback: function () {
+                $(this).css('border', '2px dotted #0B85A1');
+                $(this).text('正在上传，请稍候...')
+            },
+            uploaded_css_callback: function () {
+                $(this).css('border', '2px dotted #0B85A1');
+                $(this).text('或将图片拖拽到此处直接上传');
+            }
+
+        };
+        return $.extend({}, default_options, opts == null ? {} : opts);
+    };
+
+    $.fn.extend({
+        dragDropUploadWrapper: function (opts) {
+            opts = sanitized_options(opts);
+            return this.each(function () {
+                $(this).dragDropUpload({
+                    upload_file: function (file) {
+                        var fd = new FormData();
+                        fd.append('upload_file', file);
+                        fd.append('url', opts.image_url_sel ? $(opts.image_url_sel).val() : '');
+                        fd.append('descr', opts.image_desc_sel ? $(opts.image_desc_sel).val() : '');
+                        return (
+                            $.ajax({
+                                url: '/api/admin/image/upload_image/' + opts.image_type,
+                                type: 'POST',
+                                dataType: 'json',
+                                contentType: false,
+                                processData: false,
+                                cache: false,
+                                data: fd
+                            }).then(function (data) {
+                                return (
+                                    $.ajax({
+                                        url: '/api/admin/image/get_image_info/' + opts.image_type + '/' + data.image_id,
+                                        dataType: 'json',
+                                        cache: false
+                                    }).then(function (image_info) {
+                                        var dtd = $.Deferred();
+                                        return dtd.resolve({
+                                            url: '/uploads/images/' + opts.image_type + '/' + image_info.path + '400px/' + image_info.file_name,
+                                            id: image_info.id,
+                                            file_name: image_info.file_name
+                                        });
+                                    })
+                                );
+                            })
+                        )
+                    },
+                    upload_callback: opts.upload_callback,
+                    dragenter_css_callback: opts.dragenter_css_callback,
+                    dragover_css_callback: opts.dragover_css_callback,
+                    drop_css_callback: opts.drop_css_callback,
+                    uploading_css_callback: opts.uploading_css_callback,
+                    uploaded_css_callback: opts.uploaded_css_callback
+                });
+                
+            });
+        }
+    });
+})(jQuery);
---
+++ drag-drop-upload.js
@@ -0,0 +1,82 @@
+(function ($) {
+    var sanitized_options = function(opts) {
+        var default_options = {
+            upload_file: void 0,          // 上传文件(必填,$.ajax().then())
+            upload_callback: void 0,      // 上传成功回调函数
+            dragenter_css_callback: opts.dragenter_css_callback,    // 图片进入拖拽区域CSS样式回调函数
+            dragover_css_callback: opts.dragover_css_callback,      // 图片在拖拽区域移动时CSS样式回调函数
+            drop_css_callback: opts.drop_css_callback,              // 图片在拖拽区域松手后CSS样式回调函数
+            uploading_css_callback: opts.uploading_css_callback,    // 图片在上传过程中CSS样式回调函数
+            uploaded_css_callback: opts.uploaded_css_callback       // 图片上传成功后CSS样式回调函数
+        };
+        return $.extend({}, default_options, opts == null ? {} : opts);
+    };
+
+    $.fn.extend({
+        dragDropUpload: function (opts) {
+            opts = sanitized_options(opts);
+            return this.each(function() {
+                $(this).on('dragenter', $.proxy(function (event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    if (opts.dragenter_css_callback) {
+                        opts.dragenter_css_callback.call(this);
+                    }
+                }, this));
+
+                $(this).on('dragover', $.proxy(function (event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    if (opts.dragover_css_callback) {
+                        opts.dragover_css_callback.call(this);
+                    }
+                }, this));
+
+                $(this).on('drop', $.proxy(function (event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    if (opts.drop_css_callback) {
+                        opts.drop_css_callback.call(this);
+                    }
+
+                    if (!$(this).attr('data-is-uploading') && opts.upload_file) {
+                        if (opts.uploading_css_callback) {
+                            opts.uploading_css_callback.call(this);
+                        }
+                        $(this).attr('data-is-uploading', 'true');
+                        var files = event.originalEvent.dataTransfer.files;
+                        $.when(opts.upload_file(files[0])).then($.proxy(function (data) {
+                            if (opts.upload_callback) {
+                                opts.upload_callback.call(this, data);
+                            }
+                        }, this)).always($.proxy(function () {
+                            $(this).removeAttr('data-is-uploading');
+                            if (opts.uploaded_css_callback) {
+                                opts.uploaded_css_callback.call(this);
+                            }
+                        }, this));
+                    }
+                }, this));
+
+                // 禁止浏览器默认行为
+                $(document).on('dragenter', function (event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                });
+
+                $(document).on('dragover', function (event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    if (opts.dragover_css_callback) {
+                        opts.dragover_css_callback.call(this);
+                    }
+                });
+
+                $(document).on('drop', function (event) {
+                    event.stopPropagation();
+                    event.preventDefault();
+                });
+            });
+        }
+    });
+})(jQuery);
